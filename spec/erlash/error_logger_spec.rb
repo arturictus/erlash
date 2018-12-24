c82bdd447408594822367c9db6acf74e96c1394a
Artur Panach <arturictus@gmail.com>
2018-12-24 15:57:18 +0800

    testing triggers

--- spec/erlash/error_logger_spec.rb
+++ spec/erlash/error_logger_spec.rb
@@ -30,13 +30,29 @@ describe Erlash::ErrorLogger do
       expect { described_class.call(malformed) }.to raise_error("BOOM")
     end
     it "wellformed error is logged" do
+      expect(Erlash.logger).to receive(:error)
       expect { described_class.call(error) }.not_to raise_error
+    end
+    it "skiping" do
+      expect(Erlash.logger).not_to receive(:error)
+      expect { described_class.call(skip) }.not_to raise_error
     end
   end
   context "other env, no test no development" do
     before do
       allow(Erlash).to receive(:env).and_return('other')
     end
+    it 'malformed is raised' do
+      expect(Erlash.logger).to receive(:warn)
+      expect { described_class.call(malformed) }.not_to raise_error
+    end
+    it "wellformed error is logged" do
+      expect(Erlash.logger).to receive(:error)
+      expect { described_class.call(error) }.not_to raise_error
+    end
+    it "skiping" do
+      expect(Erlash.logger).not_to receive(:error)
+      expect { described_class.call(skip) }.not_to raise_error
+    end
   end
-
 end
